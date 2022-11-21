73f2cc4dc85758452616b8a0c8640f577c0bffab
Jijo Johnny <iamjijojohnny@live.com>
2022-11-21 23:24:50 +0530

    Create simulate-click.js

---
+++ simulate-click.js
@@ -0,0 +1,7 @@
+function simulateMouseEvents(element, eventName) {
+    var mouseEvent= document.createEvent ('MouseEvents');
+    mouseEvent.initEvent (eventName, true, true);
+    element.dispatchEvent (mouseEvent);
+}
+
+simulateMouseEvents($0,'mousedown');
